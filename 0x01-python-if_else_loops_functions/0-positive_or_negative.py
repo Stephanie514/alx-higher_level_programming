d5e70c4f2fc902f6fd87b94321e1dfa5da562cb1
Stephanie514 <chepkiruistephanie514@gmail.com>
2023-08-09 15:42:39 +0000

    task 0

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 import random
 
-
 number = random.randint(-10, 10)
-def print_number_classification(number):
 
+def print_number_classification(number):
 
     if number > 0:
         print(f"{number} is positive")
@@ -12,4 +11,4 @@ def print_number_classification(number):
         print(f"{number} is zero")
     else:
         print(f"{number} is negative")
-print_number_classification(number)
+
