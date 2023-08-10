02cdf8d5f2354c469211798be225b7b4f246852c
Stephanie514 <chepkiruistephanie514@gmail.com>
2023-08-10 20:23:21 +0000

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,12 +6,12 @@ if __name__ == "__main__":
     arguments = sys.argv[1:]
     num_arguments = len(arguments)
 
-    output = (
-        f"Number of argument{'(s)' if num_arguments != 1 else ''}: "
-        f"{num_arguments}{':' if num_arguments > 0 else '.'}"
-    )
-
-    print(output)
+    if num_arguments == 0:
+        print("0 arguments.")
+    elif num_arguments == 1:
+        print("1 argument:")
+    else:
+        print(f"{num_arguments} arguments:")
 
     for i, arg in enumerate(arguments, start=1):
         print(f"{i}: {arg}")
