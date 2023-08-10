d4955f70efc6614049df1618c63fede920d8d6d4
Stephanie514 <chepkiruistephanie514@gmail.com>
2023-08-10 19:33:08 +0000

    pycode

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 
-
-def add(a, b):
-    return a + b
-
-
 if __name__ == "__main__":
+    from add_0 import add
+
     a = 1
     b = 2
+    result = add(a, b)
 
-    ans = add(a, b)
-
-    print("{} + {} = {}".format(a, b, ans))
+    print("{} + {} = {}".format(a, b, result))
