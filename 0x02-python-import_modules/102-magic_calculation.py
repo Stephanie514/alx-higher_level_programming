825737d710be9628225baf13aaf923740ef29972
Stephanie514 <chepkiruistephanie514@gmail.com>
2023-08-11 18:09:30 +0000

    task 8

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    """Match bytecode provided by Holberton School."""
+    from magic_calculation_102 import add, sub
+    c = 0 if a < b else sub(a, b)
+    for i in range(4, 6):
+        c = add(c, i)
+    return c
