39d3107f0cd79aef226a19b22d9e5a3b6ff837f3
Stephanie514 <chepkiruistephanie514@gmail.com>
2023-08-14 10:33:58 +0000

    task 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,9 +3,9 @@
 # 11-delete_at.py
 
 def delete_at(my_list=[], idx=0):
-    """Delete an item at a specific position in a list."""
+    """Delete the item at a specific position in a list."""
     if 0 <= idx < len(my_list):
         new_list = my_list[:idx] + my_list[idx+1:]
+        return new_list
     else:
-        new_list = my_list[:]
-    return new_list
+        return my_list
