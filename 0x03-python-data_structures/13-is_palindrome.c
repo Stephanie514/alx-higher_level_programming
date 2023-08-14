b7667078626664ea7a4abeff698969716b5a521f
Stephanie514 <chepkiruistephanie514@gmail.com>
2023-08-14 12:12:39 +0000

    task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,25 +1,6 @@
 #include "lists.h"
-#include <stddef.h>
 
-/**
- * reverse_list - Reverses a singly-linked list.
- * @head: A pointer to the starting node of the list to reverse.
- * Return: A pointer to the head of the reversed list.
- */
-listint_t *reverse_list(listint_t **head)
-{
-	listint_t *prev = NULL, *current = *head, *next;
-
-	while (current != NULL)
-	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
-	*head = prev;
-	return (*head);
-}
+listint_t *reverse_listint(listint_t **head);
 
 /**
  * is_palindrome - Checks if a singly linked list is a palindrome.
@@ -29,46 +10,43 @@ listint_t *reverse_list(listint_t **head)
  */
 int is_palindrome(listint_t **head)
 {
-	if (*head == NULL || (*head)->next == NULL)
-		return (1);
-	listint_t *slow_ptr = *head, *fast_ptr = *head, *prev_slow = *head;
-	listint_t *second_half, *mid_node = NULL;
-	int is_palindrome = 1;
-
-	while (fast_ptr != NULL && fast_ptr->next != NULL)
-	{
-		fast_ptr = fast_ptr->next->next;
-		prev_slow = slow_ptr;
-		slow_ptr = slow_ptr->next;
-	}
-
-	if (fast_ptr != NULL)
-	{
-		mid_node = slow_ptr;
-		slow_ptr = slow_ptr->next;
-	}
-
-	second_half = slow_ptr;
-	prev_slow->next = NULL;
-	reverse_list(&second_half);
-	listint_t *first_half = *head, *reversed_half = second_half;
-
-	for (; first_half != NULL && reversed_half != NULL;
-			first_half = first_half->next, reversed_half = reversed_half->next)
-	{
-		if (first_half->n != reversed_half->n)
-		{
-			is_palindrome = 0;
-			break;
-		}
-	}
-	reverse_list(&second_half);
-	if (mid_node != NULL)
-	{
-		prev_slow->next = mid_node;
-		mid_node->next = second_half;
-	}
-	else
-	prev_slow->next = second_half;
-	return (is_palindrome);
+if (*head == NULL || (*head)->next == NULL)
+return (1);
+listint_t *slow = *head, *fast = *head, *prev_slow = *head;
+listint_t *second_half, *mid_node = NULL;
+int is_palindrome = 1;
+while (fast != NULL && fast->next != NULL)
+{
+fast = fast->next->next;
+prev_slow = slow;
+slow = slow->next;
+}
+if (fast != NULL)
+{
+mid_node = slow;
+slow = slow->next;
+}
+second_half = slow;
+prev_slow->next = NULL;
+reverse_listint(&second_half);
+listint_t *first_half = *head, *reversed_half = second_half;
+while (first_half != NULL && reversed_half != NULL)
+{
+if (first_half->n != reversed_half->n)
+{
+is_palindrome = 0;
+break;
+}
+first_half = first_half->next;
+reversed_half = reversed_half->next;
+}
+reverse_listint(&second_half);
+if (mid_node != NULL)
+{
+prev_slow->next = mid_node;
+mid_node->next = second_half;
+}
+else
+prev_slow->next = second_half;
+return (is_palindrome);
 }
--- 0x03-python-data_structures/linked_lists.c
+++ 0x03-python-data_structures/linked_lists.c
@@ -72,3 +72,27 @@ void free_listint(listint_t *head)
         free(current);
     }
 }
+/**
+ * reverse_listint - Reverses a singly linked listint_t list.
+ * @head: A pointer to the starting node of the list to reverse.
+ *
+ * Return: A pointer to the head of the reversed list.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+    listint_t *prev = NULL;
+    listint_t *current = *head;
+    listint_t *next = NULL;
+
+    while (current != NULL)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    *head = prev;
+
+    return *head;
+}
