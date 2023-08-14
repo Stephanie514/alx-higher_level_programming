562b9f9cc9cf32b71f16ec7fb22fb8d72cfb4cce
Stephanie514 <chepkiruistephanie514@gmail.com>
2023-08-14 08:10:26 +0000

    task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,10 +4,9 @@
 
 def replace_in_list(my_list, idx, element):
     """Replace an element of a list at a specific position."""
-    result = my_list[:]
+    new_list = my_list.copy()  # Create a copy of the original list
 
-    if 0 <= idx < len(result):
-        result.pop(idx)
-        result.insert(idx, element)
+    if 0 <= idx < len(new_list):
+        new_list[idx:idx+1] = [element]
 
-    return result
+    return new_list
