a5a2ecbedc4443b6f22d99b09408f2974ec6970a
Stephanie514 <chepkiruistephanie514@gmail.com>
2023-08-16 16:01:06 +0000

    Task 8

--- 0x04-python-more_data_structures/8-main.py
+++ 0x04-python-more_data_structures/8-main.py
@@ -1,4 +1,5 @@
-imple_delete = __import__('8-simple_delete').simple_delete
+#!/usr/bin/python3
+simple_delete = __import__('8-simple_delete').simple_delete
 print_sorted_dictionary = \
     __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
