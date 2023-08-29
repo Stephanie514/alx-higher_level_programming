8293da99272268198513e38e1171d84a39e574f9
Stephanie514 <chepkiruistephanie514@gmail.com>
2023-08-29 18:29:03 +0000

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,7 +4,7 @@
 
 
 class Square:
-    """Represent a square"""
+    """Represent class Square"""
 
     def __init__(self, size=0, position=(0, 0)):
         """
@@ -14,13 +14,13 @@ class Square:
             size (int): The size of the new square.
             position (tuple): The position of the new square.
         """
-        self.my_size = size
-        self.my_position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
         """Retrieve the current size of the square."""
-        return self.my_size
+        return self.__size
 
     @size.setter
     def size(self, value):
@@ -28,12 +28,12 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.my_size = value
+        self.__size = value
 
     @property
     def position(self):
         """Retrieve the current position of the square."""
-        return self.my_position
+        return self.__position
 
     @position.setter
     def position(self, value):
@@ -44,28 +44,28 @@ class Square:
             or not all(num >= 0 for num in value)
         ):
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.my_position = value
+        self.__position = value
 
     def area(self):
         """Return the current area of the square."""
-        return self.my_size ** 2
+        return self.size ** 2
 
     def my_print(self):
         """Print the square with the # character."""
-        if self.my_size == 0:
-            print("")
+        if self.size == 0:
+            print()
             return
 
-        for _ in range(self.my_position[1]):
+        for _ in range(self.position[1]):
             print()
-        for _ in range(self.my_size):
-            print(" " * self.my_position[0] + "#" * self.my_size)
+        for _ in range(self.size):
+            print(" " * self.position[0] + "#" * self.size)
 
 
 if __name__ == "__main__":
     my_square = Square()
-    my_square.my_size = int(input("Enter the size of the square: "))
-    my_square.my_position = tuple(
+    my_square.size = int(input("Enter the size of the square: "))
+    my_square.position = tuple(
         map(int, input("Enter the position of the square (x, y): ").split())
     )
     print("Square size:", my_square.size)
