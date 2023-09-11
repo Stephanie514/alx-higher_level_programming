8dcaf8b5535272310c9dfbeb6e6d73965d3a2c01
STEPH <chepkiruistephanie514@gmail.com>
2023-09-11 16:56:23 +0000

    Task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""MyList: A custom list class inheriting from the built-in list class."""
+
+
+class MyList(list):
+    """MyList is an extension of the list class with more functionality."""
+
+    def print_sorted(self):
+        """Prints the list in ascending order after sorting."""
+        for item in sorted(self):
+            print(item)
