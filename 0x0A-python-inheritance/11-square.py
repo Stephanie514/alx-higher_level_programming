d07a97063f177f6777288add262ee26db0a49ee6
STEPH <chepkiruistephanie514@gmail.com>
2023-09-11 21:57:35 +0000

    Task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Define square module """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square class body """
+
+    def __init__(self, size):
+        """
+        Initialize a Square instance.
+
+        Args:
+            size (int): The size of the square side.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """
+        Calculate the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """
+        Return a string representation of the square.
+
+        Returns:
+            str: A string in the format "[Square] <size>/<size>".
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
