54ac8e47e967390aca4309b17b04e21a2816423f
STEPH <chepkiruistephanie514@gmail.com>
2023-09-12 20:13:35 +0000

    Task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,14 +2,16 @@
 """Defines a module that appends a string to a text file."""
 
 
-def append_write(file_name="", content=""):
-    """
-    Appends a string to the end of a text file (UTF8) and returns the
+def append_write(file_name="", text=""):
+    """Appends a string to the end of a text file (UTF8) and returns the
     number of characters added.
     """
     try:
+        if not text.endswith("\n"):
+            text += "\n"
+
         with open(file_name, "a", encoding="utf-8") as my_file:
-            char_count = my_file.write(content)
+            char_count = my_file.write(text)
             return char_count
     except Exception as e:
         print(f"An error occurred: {e}")
