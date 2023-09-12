3b008ac4bedf3de67d6f3159bdccd984c7f72d1a
STEPH <chepkiruistephanie514@gmail.com>
2023-09-12 20:28:54 +0000

    Task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,7 +3,8 @@
 
 
 def append_write(filename="", text=""):
-    """Appends a string to the end of a text file (UTF8) and returns the
+    """
+    Appends a string to the end of a text file (UTF8) and returns the
     number of characters added.
     """
     try:
