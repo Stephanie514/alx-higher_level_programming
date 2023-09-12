96823a4f5fb4bb544b37ff0260bf8f96b21babee
STEPH <chepkiruistephanie514@gmail.com>
2023-09-12 18:47:19 +0000

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,6 +8,11 @@ This script defines a function to read a text file
 
 
 def read_file(filename=""):
-    with open(filename, 'r', encoding='utf-8') as f:
-        for line in f:
-            print(line, end='')
+    """
+    read_file function
+    Reads a text file (UTF8) and prints it to stdout.
+
+    :param filename: The name of the file to be read.
+    """
+    with open(filename, mode='r', encoding='utf-8') as f:
+        print(f.read(), end='')
