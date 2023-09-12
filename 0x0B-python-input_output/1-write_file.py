b467beddec576d3fdf47f30d2c662aa8fbd9f7eb
STEPH <chepkiruistephanie514@gmail.com>
2023-09-12 19:45:43 +0000

    Task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""Defines WriteFile module."""
+"""Defines Write_file module."""
 
 
-def write_file(file_name="", content=""):
-    """Write a string to a UTF-8 text file and return the character count."""
-    try:
-        with open(file_name, "w", encoding="utf-8") as my_file:
-            my_file.write(content)
-            char_count = len(content)
-            return char_count
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return -1
+def write_file(filename="", text=""):
+    """Write a string to a UTF8 text file.
+    """
+    char_count = len(text)  # Calculate the character count
+    with open(filename, "w", encoding="utf-8") as my_file:
+        my_file.write(text)
+    return char_count
