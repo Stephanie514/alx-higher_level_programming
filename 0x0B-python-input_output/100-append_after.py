cec15358aad3b4f6565408b372a3c21a1388ff5b
STEPH <chepkiruistephanie514@gmail.com>
2023-09-13 00:27:09 +0000

    Task 13

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,16 +4,17 @@ This module defines a function append_after that inserts a line of text into
 a file after each line containing a specific string.
 """
 
+
 def append_after(filename="", search_string="", new_string=""):
     """
     Insert a line of text into a file after each line
-containing a specific string.
+    containing a specific string.
 
     Args:
         filename (str): The name of the file to modify.
         search_string (str): The string to search for in each line.
         new_string (str): The string to insert after lines
-containing the search_string.
+        containing the search_string.
     """
     try:
         with open(filename, "r") as file:
@@ -25,7 +26,5 @@ containing the search_string.
                 if search_string in line:
                     file.write(new_string)
 
-    except FileNotFoundError:
-        print(f"File '{filename}' not found.")
     except Exception as e:
-        print(f"An error occurred: {str(e)}")
+        pass
