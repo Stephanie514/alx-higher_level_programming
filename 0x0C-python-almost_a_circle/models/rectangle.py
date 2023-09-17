532a92b0256e66eaed3fb6a14e8eb8708f5c4343
STEPH <chepkiruistephanie514@gmail.com>
2023-09-17 17:22:31 +0000

    Task 8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -108,3 +108,26 @@ class Rectangle(Base):
         """Override the __str__ method to return a formatted string."""
         return ("[Rectangle] ({}) {}/{} - {}/{}"
                 .format(self.id, self.x, self.y, self.width, self.height))
+
+    def update(self, *args):
+        """
+        Assigns arguments to each attribute in the following order:
+        1st argument should be the id attribute
+        2nd argument should be the width attribute
+        3rd argument should be the height attribute
+        4th argument should be the x attribute
+        5th argument should be the y attribute
+
+        Args:
+            *args: Variable-length arguments, in the specified order.
+        """
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
--- 0x0C-python-almost_a_circle/tests/rectangle_test.py
+++ 0x0C-python-almost_a_circle/tests/rectangle_test.py
@@ -109,6 +109,31 @@ class TestRectangle(unittest.TestCase):
         expected_str = "[Rectangle] (10) 1/2 - 4/5"
         self.assertEqual(str(r1), expected_str)
 
+    def test_update(self):
+        """Test the update method to assign values to attributes."""
+        r1 = Rectangle(4, 5, 1, 2, 10)
+        r1.update(89)
+        self.assertEqual(r1.id, 89)
+
+        r1.update(89, 2)
+        self.assertEqual(r1.width, 2)
+
+        r1.update(89, 2, 3)
+        self.assertEqual(r1.height, 3)
+
+        r1.update(89, 2, 3, 4)
+        self.assertEqual(r1.x, 4)
+
+        r1.update(89, 2, 3, 4, 5)
+        self.assertEqual(r1.y, 5)
+
+        r1.update()
+        self.assertEqual(r1.id, 89)
+        self.assertEqual(r1.width, 2)
+        self.assertEqual(r1.height, 3)
+        self.assertEqual(r1.x, 4)
+        self.assertEqual(r1.y, 5)
+
 
 if __name__ == '__main__':
     unittest.main()
