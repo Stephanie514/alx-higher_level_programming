c21087f5d2bd9f6f201f631312f6e965d5fb6e58
STEPH <chepkiruistephanie514@gmail.com>
2023-10-18 23:08:13 +0000

    Task 4

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- Script to create a table called 'first_table' in the current database
+-- This script does not specify the database name; it is provided as an argument when running the script
+
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+
