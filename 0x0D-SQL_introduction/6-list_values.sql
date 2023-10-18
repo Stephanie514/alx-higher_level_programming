5330c2c37726240201e57dd38a3bb5dc3794f7b3
STEPH <chepkiruistephanie514@gmail.com>
2023-10-18 23:14:49 +0000

    Task 6

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,4 @@
+-- Script to list all rows of the 'first_table' from the 'hbtn_0c_0' database
+-- Usage: mysql -u your_username -p your_password -e "USE hbtn_0c_0; SELECT * FROM first_table;"
+-- This script does not specify the database name; it is provided as an argument when running the script
+SELECT * FROM first_table;
