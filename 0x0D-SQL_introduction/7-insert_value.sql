ccafe29a9d3d15247999876996aff43636f95c89
STEPH <chepkiruistephanie514@gmail.com>
2023-10-18 23:18:45 +0000

    Task 7

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- Script to insert a new row into 'first_table' in the 'hbtn_0c_0' database
+-- Usage: mysql -u your_username -p your_password -e "USE hbtn_0c_0; INSERT INTO first_table (id, name) VALUES (89, 'Best School');"
+-- This script does not specify the database name; it is provided as an argument when running the script
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, 'Best School');
