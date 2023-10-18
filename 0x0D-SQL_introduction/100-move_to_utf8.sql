07e1fef20458e0afbbd1af35f8f3136c270ad878
STEPH <chepkiruistephanie514@gmail.com>
2023-10-18 23:59:00 +0000

    Task 17

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,5 @@
+-- Script to convert the hbtn_0c_0 database, first_table, and the name field to UTF8
+-- This script does not specify the database name; it is provided as an argument when running the script
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
