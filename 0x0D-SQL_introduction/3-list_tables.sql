ec8ff5f0d7bfeabf9304f44542549bea47f1bb33
STEPH <chepkiruistephanie514@gmail.com>
2023-10-18 22:56:44 +0000

    Task 3

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,3 +1,3 @@
 -- Script to list all tables in a specified database
 -- Drop the database 'hbtn_0c_0' if it exists; if it doesn't exist, the script will not fail
-DROP SCHEMA IF EXISTS hbtn_0c_0;
+DROP DATABASE IF EXISTS hbtn_0c_0;
