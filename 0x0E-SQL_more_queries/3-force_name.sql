1d1fcf87bc4f292f01b15d8f8e0a2e109673e02a
STEPH <chepkiruistephanie514@gmail.com>
2023-10-19 17:31:18 +0000

    Task 3

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,17 +1,3 @@
 -- Create the force_name table in the specified database
 -- Table description: id INT, name VARCHAR(256) (not null)
--- The database name will be passed as an argument to the mysql command
--- Replace 'your_database_name' with the actual database name
-
-USE your_database_name
-
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT,
-    name VARCHAR(256) NOT NULL
-);
--- Insert initial data if the table doesn't exist
--- You can modify the values as needed
-
-INSERT IGNORE INTO force_name (id, name)
-VALUES (1, 'ExampleName1'),
-       (2, 'ExampleName2');
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
