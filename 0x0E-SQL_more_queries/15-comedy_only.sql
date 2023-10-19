ec3760f3fa95b24441b1f3e728a9fafaab064789
STEPH <chepkiruistephanie514@gmail.com>
2023-10-19 19:49:42 +0000

    Task 15

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,11 +1,9 @@
 -- List all Comedy shows from the hbtn_0d_tvshows database.
 
-USE hbtn_0d_tvshows;
-
-SELECT tv_shows.title
+SELECT title
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = 'Comedy'
-GROUP BY tv_shows.title
-ORDER BY tv_shows.title ASC;
+GROUP BY title
+ORDER BY title ASC;
