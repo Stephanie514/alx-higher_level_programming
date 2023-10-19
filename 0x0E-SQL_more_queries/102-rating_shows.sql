7a2cac515511a2977e5fd37b09e1b640bd201dd2
STEPH <chepkiruistephanie514@gmail.com>
2023-10-19 20:24:16 +0000

    Task 19

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,9 +1,6 @@
 -- Retrieve and list all TV shows from the hbtn_0d_tvshows_rate database, sorted by their overall ratings.
-
-USE hbtn_0d_tvshows_rate;
-
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS 'rating'
+SELECT title, SUM(tv_show_ratings.rate) 'rating'
 FROM tv_shows
 LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
-GROUP BY tv_shows.title
+GROUP BY title
 ORDER BY rating DESC;
