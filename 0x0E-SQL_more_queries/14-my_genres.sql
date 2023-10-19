90be5e03d412bf6a4efeab8e263e4bde7cb6d4a5
STEPH <chepkiruistephanie514@gmail.com>
2023-10-19 19:38:03 +0000

    Task 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,10 +1,8 @@
 -- List all TV show genres and the number of shows linked to each genre.
-
-USE hbtn_0d_tvshows;
-
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+SELECT name
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY genre
-HAVING number_of_shows > 0
-ORDER BY number_of_shows DESC;
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter'
+GROUP BY name
+ORDER BY name ASC;
