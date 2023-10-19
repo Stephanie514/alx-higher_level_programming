3c47158219c56a0213dcd1e988122ce4985ee5f1
STEPH <chepkiruistephanie514@gmail.com>
2023-10-19 18:10:51 +0000

    Task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,4 @@
 -- Create the 'unique_id' table in your MySQL server.
 -- This table definition includes an 'id' column with a default value of 1
 -- and a unique constraint, along with a 'name' column.
-
-CREATE TABLE IF NOT EXISTS unique_id (
-    id INT DEFAULT 1 UNIQUE,
-    name VARCHAR(256)
-);
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
