d83031651d160c630bdcf7021ae0bbabeb6820c9
STEPH <chepkiruistephanie514@gmail.com>
2023-10-19 18:40:39 +0000

    Task 8

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- List all the cities of California from the hbtn_0d_usa database
+-- without using JOIN and sort the results in ascending order by cities.id.
+
+USE hbtn_0d_usa;
+
+SELECT id, name
+FROM cities
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+ORDER BY id ASC;
