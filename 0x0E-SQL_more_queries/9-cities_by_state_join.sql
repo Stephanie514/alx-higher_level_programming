843a4ff76873296d8b99ff251f8ffad427953b78
STEPH <chepkiruistephanie514@gmail.com>
2023-10-19 19:00:39 +0000

    Task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,8 +1,3 @@
 -- List all cities in the hbtn_0d_usa database along with their states.
  -- Replace with the actual database name
-USE hbtn_0d_usa;
-
-SELECT CONCAT(cities.id, ' - ', cities.name, ' - ', states.name) AS CityInfo
-FROM cities
-LEFT JOIN states ON states.id = cities.state_id
-ORDER BY cities.id;
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state_id ORDER BY cities.id;
