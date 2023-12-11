09bc4c6ce19edeeee2b931bce4e22feec22b80c3
STEPH <chepkiruistephanie514@gmail.com>
2023-12-11 22:58:46 +0000

    Task 17

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 const myObject = {
   type: 'object',
-  value: 12,
-  incr() {
-    this.value++;
-  }
+  value: 12
 };
-
 console.log(myObject);
+myObject.incr = function () {
+  this.value++;
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
