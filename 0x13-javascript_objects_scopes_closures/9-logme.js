17339f72c42359fc51721cdb327f948f73c7152c
STEPH <chepkiruistephanie514@gmail.com>
2023-12-13 19:35:00 +0000

    Task 9

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,7 +1,9 @@
-exports.logMe = (function() {
-  let Count = 0;
-  return function(item) {
-    console.log(Count + ': ' + item);
-    Count++;
-  };
-})();
+#!/usr/bin/node
+
+exports.logMe = function (item) {
+  if (typeof this.ttl === 'undefined') {
+    this.ttl = 0;
+  }
+  console.log(this.ttl + ': ' + item);
+  this.ttl++;
+};
