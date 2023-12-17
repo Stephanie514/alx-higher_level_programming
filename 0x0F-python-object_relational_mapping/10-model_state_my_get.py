69aee7883067aae5a2258c562dc44db440cdd96e
STEPH <chepkiruistephanie514@gmail.com>
2023-12-17 17:42:32 +0000

    Task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
 
-""" 10-model_state_my_get module """
-
-import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5 or ";" in sys.argv[4]:
-        exit(1)
+    if len(sys.argv) != 5:
+        sys.exit("Usage: ./script.py username password database_name state_name")
 
     username, password, database, state_name = sys.argv[1:5]
 
     connection_str = f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}"
+
     engine = create_engine(connection_str)
     Session = sessionmaker(bind=engine)
-
-    Base.metadata.create_all(engine)
 
     session = Session()
 
@@ -27,5 +24,4 @@ if __name__ == "__main__":
         print("Not found")
     else:
         print(state_query.id)
-
     session.close()
