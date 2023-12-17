027e78d0a70e074b59ae95619f00448157455522
STEPH <chepkiruistephanie514@gmail.com>
2023-12-17 20:03:43 +0000

    Task 11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument
+""" This prints the State object with the name passed as argument
 """
+
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
@@ -8,7 +9,7 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        exit(1)
+        sys.exit(1)
 
     username, password, database = sys.argv[1:4]
 
@@ -21,7 +22,10 @@ if __name__ == "__main__":
     session.add(new_state)
     session.commit()
 
-    new_instance = session.query(State).filter_by(name='Louisiana').first()
-    print(new_instance.id)
+    new_instance = session.query(State).filter(State.name == 'Louisiana').first()
+    if new_instance is None:
+        print("Not found")
+    else:
+        print(new_instance.id)
 
     session.close()
