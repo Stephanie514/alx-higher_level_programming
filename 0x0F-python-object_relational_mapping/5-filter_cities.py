6497804d4a77b311569c74e250d340b211efe512
STEPH <chepkiruistephanie514@gmail.com>
2023-12-17 16:38:21 +0000

    Task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -5,8 +5,9 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        sys.exit("Usage: ./script.py username password database_name state_name")
-    
+        sys.exit("Usage: ./script.py username password database_name "
+                 "state_name")
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
@@ -15,21 +16,21 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=username,
                          passwd=password, db=database, port=3306)
     cursor = db.cursor()
-    
+
     qry = """
-        SELECT cities.name 
-        FROM cities 
-        INNER JOIN states 
+        SELECT cities.name
+        FROM cities
+        INNER JOIN states
         ON states.id=cities.state_id
         WHERE states.name=%s
         ORDER BY cities.id ASC
     """
-    
+
     cursor.execute(qry, (state_name,))
     rows = cursor.fetchall()
-    
+
     city_names = [row[0] for row in rows]
     print(", ".join(city_names))
-    
+
     cursor.close()
     db.close()
