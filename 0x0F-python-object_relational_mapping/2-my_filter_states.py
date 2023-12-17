975eb3115a04b845157995e9d0843781ba3c69ab
STEPH <chepkiruistephanie514@gmail.com>
2023-12-17 15:17:58 +0000

    Task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""This displays all values in the states table of hbtn_0e_0_usa where name matches the argument"""
+"""This displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
         sys.exit("Usage: ./script.py username password database_name state_name")
-    
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
@@ -15,13 +16,13 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=username,
                          passwd=password, db=database, port=3306)
     cursor = db.cursor()
-    
+
     query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC".format(state_name)
     cursor.execute(query)
-    
+
     rows = cursor.fetchall()
     for row in rows:
         print(row)
-    
+
     cursor.close()
     db.close()
