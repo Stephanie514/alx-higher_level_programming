8ad914e9aa787a32ab061c8be22bcd3a9dcf5f88
STEPH <chepkiruistephanie514@gmail.com>
2023-12-18 18:07:02 +0000

    Task 16

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-""" Lists all State objects and corresponding City objects contained in the database """
+""" Lists all State objects and corresponding City objects
+contained in the database """
 
 import sys
 from sqlalchemy import create_engine
@@ -8,22 +9,33 @@ from sqlalchemy.orm import sessionmaker
 from relationship_city import City
 from relationship_state import Base, State
 
-if __name__ == "__main__":
+
+def create_engine_session(username, password, database_name):
+    conn_str = f"mysql+mysqldb://{username}:{password}@localhost:3306/{database_name}"
+    engine = create_engine(conn_str)
+    Session = sessionmaker(bind=engine)
+    return Session()
+
+
+def display_states_cities(session):
+    query_result = session.query(State).order_by(State.id).all()
+    for state in query_result:
+        print(f"{state.id}: {state.name}")
+        for city in state.cities:
+            print(f"\t{city.id}: {city.name}")
+
+
+def main():
     if len(sys.argv) != 4:
         print("Usage: {} username password database_name".format(sys.argv[0]))
         sys.exit(1)
 
     username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
-    connection_string = f"mysql+mysqldb://{username}:{password}@localhost:3306/{database_name}"
-    engine = create_engine(connection_string)
-    Session = sessionmaker(bind=engine)
+    session = create_engine_session(username, password, database_name)
+    Base.metadata.create_all(session.get_bind())
+    display_states_cities(session)
+    session.close()
 
-    Base.metadata.create_all(engine)
 
-    with Session() as session:
-        query_result = session.query(State).order_by(State.id).all()
-
-        for state in query_result:
-            print("{}: {}".format(state.id, state.name))
-            for city in state.cities:
-                print("\t{}: {}".format(city.id, city.name))
+if __name__ == "__main__":
+    main()
