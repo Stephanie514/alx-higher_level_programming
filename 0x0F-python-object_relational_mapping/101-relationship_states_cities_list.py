7f926fb55bc8b117a915ab235eb0e8c925b5e058
STEPH <chepkiruistephanie514@gmail.com>
2023-12-18 18:17:01 +0000

    Task 16

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""Lists all State objects and corresponding City objects contained in the database."""
+""" Lists all State objects and corresponding City objects contained in the database """
 
 import sys
 from sqlalchemy import create_engine
@@ -8,33 +8,22 @@ from sqlalchemy.orm import sessionmaker
 from relationship_city import City
 from relationship_state import Base, State
 
-
-def create_engine_session(username, password, database_name):
-    conn_str = f"mysql+mysqldb://{username}:{password}@localhost:3306/{database_name}"
-    engine = create_engine(conn_str)
-    Session = sessionmaker(bind=engine)
-    return Session()
-
-
-def display_states_cities(session):
-    query_result = session.query(State).order_by(State.id).all()
-    for state in query_result:
-        print(f"{state.id}: {state.name}")
-        for city in state.cities:
-            print(f"\t{city.id}: {city.name}")
-
-
-def main():
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database_name".format(sys.argv[0]))
-        sys.exit(1)
-
-    username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
-    session = create_engine_session(username, password, database_name)
-    Base.metadata.create_all(session.get_bind())
-    display_states_cities(session)
-    session.close()
-
-
 if __name__ == "__main__":
-    main()
+    INPUT = sys.argv
+    if len(INPUT) < 4:
+        exit(1)
+
+    conn_str = "mysql+mysqldb://{}:{}@localhost:3306/{}"
+    engine = create_engine(conn_str.format(INPUT[1], INPUT[2], INPUT[3]))
+    Session = sessionmaker(bind=engine)
+
+    Base.metadata.create_all(engine)
+
+    session = Session()
+    my_query = session.query(State).order_by(State.id).all()
+    for state, *_ in my_query:
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("\t{}: {}".format(city.id, city.name))
+
+    session.close()
