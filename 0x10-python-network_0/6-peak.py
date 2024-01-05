7fdef2e8d3057243530e05e0fd590bfc8d63f050
STEPH <chepkiruistephanie514@gmail.com>
2024-01-05 21:19:41 +0000

    Task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""contains the function find_peak"""
+
+
+def find_peak(list_of_integers):
+    if not list_of_integers:
+        return None
+
+    low, high = 0, len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+
+    return list_of_integers[low]
