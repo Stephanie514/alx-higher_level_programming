49ffbf61263be3a9c25771e8c0080fb53aa49813
STEPH <chepkiruistephanie514@gmail.com>
2024-01-05 18:13:45 +0000

    Task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #takes in a URL and displays all HTTP methods the server will accept.
-curl -s -X OPTIONS -i "$1" | awk '/Allow:/ {$1=""; print $0}'
+curl -s -X OPTIONS -i "$1" | grep -i '^Allow:' | cut -d ' ' -f 2-
