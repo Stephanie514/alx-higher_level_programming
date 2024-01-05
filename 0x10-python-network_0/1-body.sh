a3bd539fc1570f7135a79aea8b749306da927c58
STEPH <chepkiruistephanie514@gmail.com>
2024-01-05 17:52:45 +0000

    Task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #This sends a GET request to URL with curl and displays body of the response
-curl -s -o response.txt -w "%{http_code}" "$1" && [ "$(cat response.txt)" = "200" ] && tail -n +2 response.txt
+curl -sL "$1"
