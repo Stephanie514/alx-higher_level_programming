38ec95fb2e023fc9e89f8ba0fa5e940ce4ce0bfa
STEPH <chepkiruistephanie514@gmail.com>
2024-01-07 22:57:31 +0000

    Task 10

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Python script that takes 2 arguments in order to solve this challenge.
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    repository = sys.argv[1]
+    owner = sys.argv[2]
+
+    url = f"https://api.github.com/repos/{owner}/{repository}/commits"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        commits = response.json()
+        for commit in commits[:10]:
+            sha = commit.get("sha")
+            author_name = commit.get("commit").get("author").get("name")
+            print(f"{sha}: {author_name}")
+    else:
+        print("Error fetching commits. Status code:", response.status_code)
