86bb07a7254e133e18732aadd7a9c7c91f9d46be
STEPH <chepkiruistephanie514@gmail.com>
2024-01-07 21:57:52 +0000

    Task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Python script that takes in a URL, sends a request
 to the URL and displays the body of the response
-(decoded in utf-8) 
+(decoded in utf-8)
   - Handles HTTP errors.
 """
 import sys
