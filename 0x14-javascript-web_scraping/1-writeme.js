444c3a010f937dc66663b50e876c048bbfcf022b
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-01-30 17:20:02 +0000

    Task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,14 +2,8 @@
 
 const fs = require('fs');
 
-const args = process.argv;
-const filePath = args[2];
-const contentToWrite = args[3];
+const [,, filePath, contentToWrite] = process.argv;
 
 fs.writeFile(filePath, contentToWrite, 'utf8', (err) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`Content written to ${filePath}`);
+  if (err) console.error(err);
 });
