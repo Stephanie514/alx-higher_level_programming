34d7e23e65efdbb4bde9c7556c05fd4be29bc0ba
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-01-30 17:26:48 +0000

    Task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request.get(url, (error, response) => {
+  if (error) return console.error(error);
+  console.log(`code: ${response.statuscode}`);
+});
