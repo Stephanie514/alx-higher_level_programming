8ccd0dc8d16edf62326c76ba8c9c9da2162f1bfa
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-01-30 18:32:51 +0000

    Task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -10,18 +10,19 @@ const filePath = args[3];
 request.get(url, (error, response, body) => {
   if (error) {
     console.error(error);
-    return;
+    process.exit(1);
   }
 
   if (response.statusCode === 200) {
     fs.writeFile(filePath, body, 'utf8', (error) => {
       if (error) {
         console.error(error);
-      } else {
-        console.log(`Content saved to ${filePath}`);
+        process.exit(1);
       }
+      console.log(`Content saved to ${filePath}`);
     });
   } else {
     console.error(`Error: ${response.statusCode} - ${response.statusMessage}`);
+    process.exit(1);
   }
 });
